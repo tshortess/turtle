ee156db27c369887bbf3fa873aa04b81210b002c
Timothy Shortess <timothy.shortess@gmail.com>
2022-06-18 21:04:43 -0500

    Added name variable and toJsonObject method to store model

--- server/src/main/java/com/topbloc/model/Store.java
+++ server/src/main/java/com/topbloc/model/Store.java
@@ -1,13 +1,30 @@
 package com.topbloc.model;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 
 public class Store {
 
+    private String name;
+
     private HashMap<Candy, Inventory> storeCandyInventory;
 
-    public Store(HashMap<Candy, Inventory> hashMapOfInventory) {
+    public Store(){
+    }
+
+    public Store(String name, HashMap<Candy, Inventory> hashMapOfInventory) {
+        this.name = name;
         this.storeCandyInventory = hashMapOfInventory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public HashMap<Candy, Inventory> getStoreCandyInventory() {
@@ -16,5 +33,26 @@ public class Store {
 
     public void setStoreCandyInventory(HashMap<Candy, Inventory> storeCandyInventory) {
         this.storeCandyInventory = storeCandyInventory;
+    }
+
+    public JSONObject toJsonObject() {
+        JSONObject returnJsonObject = new JSONObject();
+        returnJsonObject.put("name", getName());
+
+        if (getStoreCandyInventory() != null) {
+            JSONArray candyInventoryArray = new JSONArray();
+
+            getStoreCandyInventory().forEach((key, value) -> {
+                JSONObject candyItemObject = new JSONObject();
+                candyItemObject.put("candyName", key.getName());
+                candyItemObject.put("sku", String.valueOf(key.getSku()));
+                candyItemObject.put("inStockAmount", String.valueOf(value.getInStockAmount()));
+                candyItemObject.put("maxCapacity", String.valueOf(value.getMaxCapacity()));
+                candyInventoryArray.put(candyItemObject);
+            });
+
+            returnJsonObject.put("inventory", candyInventoryArray);
+        }
+        return returnJsonObject;
     }
 }
