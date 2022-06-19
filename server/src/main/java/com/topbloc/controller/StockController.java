824b03c182760f99160773c2dba6d6ed53707b4f
Timothy Shortess <timothy.shortess@gmail.com>
2022-06-18 21:06:33 -0500

    Added constructor, PoiStockDao variable, and controller endpoint methods to route to DAO

--- server/src/main/java/com/topbloc/controller/StockController.java
+++ server/src/main/java/com/topbloc/controller/StockController.java
@@ -1,6 +1,26 @@
 package com.topbloc.controller;
 
+import com.topbloc.dao.PoiStockDao;
+import com.topbloc.model.Store;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+
 public class StockController {
+
+    private PoiStockDao poiStockDao;
+
+    public StockController(PoiStockDao poiStockDao) {
+        this.poiStockDao = poiStockDao;
+    }
+
+    public JSONObject getLowStockCandy(String store) {
+        return poiStockDao.getLowStockCandy(store);
+    }
+
+    public BigDecimal getReorderCost(String orderQuantities) {
+        return poiStockDao.getReorderCost(orderQuantities);
+    }
 
 
 
